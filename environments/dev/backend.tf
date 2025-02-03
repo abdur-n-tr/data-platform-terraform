241f467f06b255518269d8abe6add5889147791a
abdurrehman <abdurrehman245@gmail.com>
2025-02-03 11:19:49 +0000

    changed config

--- environments/dev/backend.tf
+++ environments/dev/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "gcs" {
-    bucket         = "terraform-backend-dev-calm-seeker-443408-f2"
+    bucket         = "terraform-backend-dev-prj-udp-dev-qic"
     prefix         = "state"
   }
 }
